eaa8f1fb7b3f458495048ff110e9f4a7a272485d
XD <73288327+TalentGaming12@users.noreply.github.com>
2022-06-28 19:11:31 -0700

    Add files via upload

---
+++ esx_crack_applestore + MLO/Readme.txt
@@ -0,0 +1,5 @@
+author 'Crack#9684'
+
+Optimized 0.1 ms apple store fivem
+best apple store
+
---
+++ esx_crack_applestore + MLO/esx_crack_applestore/locales/en.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+	['shop'] = 'Apple Store',
+	['shops'] = 'Apple Store',
+	['press_menu'] = 'press ~INPUT_CONTEXT~ to access the apple store',
+	['bought'] = 'you just bought Iphone 13 Pro max for 10000',
+	['not_enough'] = 'you do not have enough money, you are ~missing $%s~!',
+	['player_cannot_hold'] = 'you do ~r~not~s~ have enough ~y~free space~s~ in your inventory!',
+	['shop_confirm'] = 'buy %sx %s for $%s?',
+	['no'] = 'no',
+	['yes'] = 'yes',
+}
